b458885e0f962aae89562b7bfa2ddba8a5c7b39a
monaf horany <monafhorany@gmail.com>
2024-06-06 17:14:10 +0300

    fix import model

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-
+  base: "iphone_15_landing_page",
   build: {
     sourcemap: true,
   },
